f7ac513392d2d65ba61180162c56ac57a94cb4c8
Mrichk0 <zaq4769@gmail.com>
2022-04-18 15:18:51 +0300

    Update task-09.js

--- src/js/task-09.js
+++ src/js/task-09.js
@@ -1,17 +1,14 @@
-const btnChangeColor = document.querySelector('.change-color');
-const nameColor = document.querySelector('.color');
-const body = document.querySelector('body');
+const btnChangeColor = document.querySelector(".change-color");
+const nameColor = document.querySelector(".color");
+const body = document.querySelector("body");
 
-btnChangeColor.addEventListener('click', onChangeBgColor);
+btnChangeColor.addEventListener("click", onChangeBgColor);
 
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
-};
+}
 
 function onChangeBgColor(event) {
   nameColor.textContent = getRandomHexColor();
-  body.style.backgroundColor = getRandomHexColor(); 
-};
-
-
-
+  body.style.backgroundColor = getRandomHexColor();
+}
